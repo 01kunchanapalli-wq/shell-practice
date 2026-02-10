992d796e781c7456c0f9a3338b9ae1e56b9713ba
ram <ramkunchanapalli19@gmail.com>
2026-02-10 12:31:14 +0530

    some message.

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=JOHN
+PERSON2=Raheem
+echo "$PERSON1:: hi $PERSON2,how are you?"
+echo "$PERSON2:: hi $PERSON1, im fine"
+echo "$PERSON1:: what are you learning recently?"
+echp "$PERSON12:: I'm learning shell scripting $PERSON1"
---
+++ 03-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hi suresh,how are you?"
+echo "Suresh:: hi ramesh, im fine"
+echo "Ramesh:: what are you learning recently?"
+echp "Suresh:: I'm learning shell scripting"
