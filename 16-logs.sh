4127a99b56c4fc6592423ad733f2ced8bdebbee0
ram <ramkunchanapalli19@gmail.com>
2026-03-12 19:28:17 +0530

    variables

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,11 +6,46 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER="/var/log/shell-script"
 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)"
 if [ $USERID -ne 0 ]; then
    echo  "ERROR:: Please run this script with root previlige"
    exit 1 # failure is other than 0
    fi
 
-   mkdir -p $LOGS_FOLDER
+   VALIDATE(){
+    if [ $? -ne 0 ]; then
+    echo -e"Installing $2 ... $R Failure $N"
+    exit 1
+else
+    echo -e "Installing $2 ... $G SUCCESS $N"
+fi
+}
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+  dnf install mysql -y
+  VALIDATE $? "MYSQL"
+else
+  echo -e "MySql already exist ... $Y SKIPPING $N"
+  fi 
+
+  dnf list installed nginx &>>$LOG_FILE
+  if [ $? -ne 0 ]; then
+dnf install nginx -y
+VALIDATE $? "Nginx"
+  else 
+   echo -e "nginx already exist ... $Y SKIPPING $N"
+fi
+
+   dnf list installed pyton3 &>>$LOG_FILE
+   if [ $? -ne 0 ]; then
+       dnf install python3 -y
+       VALIDATE $? "python3"
+else 
+  echo -e "python3 already exist ... $Y SKIPPING $N"
+fi
