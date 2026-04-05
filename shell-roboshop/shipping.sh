24b5cf2ec50e9a804f5df421b2af93d3005053a5
ram <ramkunchanapalli19@gmail.com>
2026-04-05 12:59:33 +0530

    session-06

--- shell-roboshop/shipping.sh
+++ shell-roboshop/shipping.sh
@@ -23,7 +23,7 @@ if [ $USERID -ne 0 ]; then
 
    VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo -e"$2 ... $R Failure $N" | tee -a $LOG_FILE
+    echo -e "$2 ... $R Failure $N" | tee -a $LOG_FILE
     exit 1
 else
     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
